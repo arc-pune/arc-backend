60782bd90318348b9ca8eaffa391d7d009549e6c
majjikishore007 <majjikishore031@gmail.com>
2021-10-03 11:56:33 +0530

    updated repo

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,4 +1,4 @@
-# Contributing to Stackoverflow Clone
+# Contributing to ARC-backend
 We love your input! We want to make contributing to this project as easy and transparent as possible, whether it's:
 
 - Reporting a bug
--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require("express");
+const app = express();
 
-var app = express();
+const DB_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT || 4000;
+/**
+ * routes
+ */
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// middlewares
+var cookieParser = require("cookie-parser");
+var morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.options("/volunteer/*", cors());
+app.use(cors());
+app.use(morgan(process.env.NODE_ENV == "production" ? "common" : "dev"));
+app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/admin", (req, res) => {
+  console.log("hulla ur on admin page");
+});
+app.get("/", (req, res) => {
+  console.log("app get ");
+  res.send("hello world");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//DB connection
+mongoose
+  .connect(DB_URL, {
+    dbName: process.env.NODE_ENV === "production" ? "prod" : "staging",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("✅ Database Connected!");
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(PORT, () => {
+  console.log("🚀 Server Ready! at port:", PORT);
 });
 
 module.exports = app;
